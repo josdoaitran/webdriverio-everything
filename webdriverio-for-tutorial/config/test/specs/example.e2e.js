b34e90911a930ab66ac2d4d7a030828c0c670763
doai.tran <doai.tran@Doai-MacbookPro.local>
2022-11-14 00:09:54 +0700

    update test example

--- webdriverio-for-tutorial/config/test/specs/example.e2e.js
+++ webdriverio-for-tutorial/config/test/specs/example.e2e.js
@@ -10,6 +10,14 @@ describe('My Login application', () => {
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
     });
+
+    it('should logout and see the alert message', async() =>{
+        await SecurePage.logout();
+        await expect(LoginPage.flashAlert).toBeExisting();
+        await expect(LoginPage.flashAlert).toHaveTextContaining(
+            'You logged out of the secure area!');
+
+    });
 });
 
 
